40fe361f447ebd70928f629dc1cf36b661940376
kanav789 <kanavrana91@gmail.com>
2025-02-04 14:21:29 +0530

    creating the basic function for greeting

---
+++ Firstpart/function.js
@@ -0,0 +1,5 @@
+"use strict";
+const greet = (h) => {
+    console.log(typeof h);
+};
+greet("Hello World");
---
+++ Firstpart/function.ts
@@ -0,0 +1,4 @@
+const greet = (h: any) => {
+    console.log(typeof h);
+};
+greet("Hello World");
